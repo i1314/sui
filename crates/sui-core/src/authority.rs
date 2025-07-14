6b558514d76e396a17b6cc644acbb0b018c60fdc
wi <w@Mac-Pro-c.local>
2025-07-14 15:35:08 +0800

    send_tx_effects_and_events

--- crates/sui-core/src/authority.rs
+++ crates/sui-core/src/authority.rs
@@ -1727,6 +1727,41 @@ impl AuthorityState {
                 .force_reload_system_packages(&BuiltInFramework::all_package_ids());
         }
 
+        let raw_events = transaction_outputs.events.clone();
+
+        let sui_events: Vec<SuiEvent> = raw_events
+            .data
+            .iter()
+            .enumerate()
+            .map(|(seq, event)| {
+                let mut layout_resolver = epoch_store.executor().type_layout_resolver(Box::new(
+                    PackageStoreWithFallback::new(
+                        &inner_temporary_store,
+                        self.get_backing_package_store(),
+                    ),
+                ));
+                let layout = layout_resolver.get_annotated_layout(&event.type_)?;
+                SuiEvent::try_from(
+                    event.clone(),
+                    *certificate.digest(),
+                    seq as u64,
+                    None,
+                    layout,
+                )
+            })
+            .collect::<Result<_, _>>()?;
+
+        if !certificate.transaction_data().is_system_tx()
+            && !sui_events.is_empty()
+            && !transaction_outputs.written.is_empty()
+        {
+            let effects = transaction_outputs.effects.clone();
+            let _ = self
+                .tx_handler
+                .send_tx_effects_and_events(effects, sui_events)
+                .await;
+        }
+
         match self.execution_scheduler.as_ref() {
             ExecutionSchedulerWrapper::ExecutionScheduler(_) => {}
             ExecutionSchedulerWrapper::TransactionManager(manager) => {
